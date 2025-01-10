73fbdd409586dde9631fe0502e736fc445ebb0c7
Mateus Stroppa <mateus_stro@hotmail.com>
2025-01-10 10:23:08 -0300

    Adicionada a função que valida quantos jogos estão alugados

--- js/app.js
+++ js/app.js
@@ -1,33 +1,35 @@
+//Declarando a variável para contar a quantidade de jogos alugados
+let jogosAlugados = 0;
 
-//Criar uma função que altere o botão e a claridade do jogo, dependendo do status dele
+// Função que imprime no console, quantos jogos foram alugados
+function contarJogosAlugados(){
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
+
+//Função que altera as classes da imagem e botão, caso ele seja alugado ou não
 function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
     let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
-    console.log(nomeJogo.textContent);
-
-
 
     //Valida se o jogo está alugado ou não, antes de alterar o status 
     if(imagem.classList.contains('dashboard__item__img--rented') && botao.classList.contains('dashboard__item__button--return')){
         //Caso o jogo esteja alugado, confirma se o usuário quer devolvê-lo
-        validacao = prompt(`Devolver o jogo ${nomeJogo.textContent}?`)
-        if(validacao == 'sim' || validacao == 'Sim' || validacao == 'SIM'){
+        if(confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}`)){
             imagem.classList.remove('dashboard__item__img--rented');
             botao.classList.remove('dashboard__item__button--return');
             botao.innerHTML = 'Alugar';
+            jogosAlugados --;
         }
     }else{
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.innerHTML = 'Devolver';
+        jogosAlugados++;
     }
-}
 
-//Função para confirmar se o usuário quer devolver o jogo
-function validarDevolucao(){
+    contarJogosAlugados();
 
-
-    
 }
